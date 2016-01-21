5807790547dc87e3f8f07ad79041fc46a7499eb2
Erik Stambaugh <erik@erikstambaugh.com>
2016-01-20 20:36:57 -0800

    just a minor tweak

--- display/DisplayDemo.cs
+++ display/DisplayDemo.cs
@@ -22,10 +22,10 @@ namespace ThugLib
                 d.DrawAt(x,22,"-",192,0,255);
             }
 
-            d.DrawAt(56,3,"5 targets in range    ",255,255,192,0,0,0);
-            d.DrawAt(75,3,"[T]",192,192,255,0,0,0);
-            d.DrawAt(60,22,"Press     for help",255,255,192,0,0,0);
-            d.DrawAt(66,22,"[?]",192,192,255,0,0,0);
+            d.DrawAt(54,3," 5 targets in range    ",255,255,192,0,0,0);
+            d.DrawAt(74,3,"[T] ",192,192,255,0,0,0);
+            d.DrawAt(58,22," Press     for help ",255,255,192,0,0,0);
+            d.DrawAt(65,22,"[?]",192,192,255,0,0,0);
 
             d.DrawScreen();
         }
