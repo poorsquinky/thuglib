07d2291d1e37cca5e71384cebf4bf0fca3e2b69c
jpmodisette <jpmodisette@gmail.com>
2016-01-27 12:49:36 -0800

    Stepped movement

--- Unity/PlayerScript.cs
+++ Unity/PlayerScript.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System;
+using System.IO;
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
@@ -7,17 +9,103 @@ public class PlayerScript : MonoBehaviour {
     private Vector3 movingFrom = new Vector3(0,0,0);
     private Vector3 movingTo   = new Vector3(0,0,0);
 
+    private long keyDownHoldStart = 0L; // in 100-ns ticks
+    private const long keyDownTimeToRepeatInMS = 300;
+    private const long keyDownRepeatIntervalInMS = 100;
+    private const long TICKS_PER_MS = 10000;
+    private bool keyIsDown = false;
+    private bool keyIsRepeating = false;
+    private int nKeyRepeats = 0;
+        
+        private void StartKeyDown()
+        {
+            keyDownHoldStart = Environment.TickCount;
+            keyIsDown = true;
+            nKeyRepeats = 0;
+        }
+
     // Use this for initialization
     void Start () {
 
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (!moving)
         {
-            float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
+            // float h = Input.GetAxis("Horizontal");
+            // float v = Input.GetAxis("Vertical");
+            float h = 0, v = 0;
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                v = 1;
+                StartKeyDown();
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                v = -1;
+                StartKeyDown();
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                h = -1;
+                StartKeyDown();
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                h = 1;
+                StartKeyDown();
+            }
+            else if (keyIsDown)
+            {
+                // check for hold
+                float possible_h = 0, possible_v = 0;
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    possible_h = -1;
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    possible_h = 1;
+                }
+                else if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    possible_v = 1;
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    possible_v = -1;
+                }
+                if (Input.GetKey(KeyCode.LeftArrow) ||
+                   Input.GetKey(KeyCode.RightArrow) ||
+                   Input.GetKey(KeyCode.UpArrow) ||
+                   Input.GetKey(KeyCode.DownArrow))
+                {
+                    long currentTick = Environment.TickCount;
+                    if (!keyIsRepeating &&
+                       currentTick - keyDownHoldStart > keyDownTimeToRepeatInMS)
+                    {
+                        keyIsRepeating = true;
+                        nKeyRepeats = 1;
+                        h = possible_h;
+                        v = possible_v;
+                    }
+                    else if (keyIsRepeating &&
+                       currentTick - keyDownHoldStart > keyDownTimeToRepeatInMS
+                       + nKeyRepeats * keyDownRepeatIntervalInMS)
+                    {
+                        nKeyRepeats++;
+                        h = possible_h;
+                        v = possible_v;
+                    }
+                }
+                else
+                {
+                    keyIsDown = false;
+                    keyIsRepeating = false;
+                    nKeyRepeats = 0;
+                }
+            }
 
             movingFrom = transform.position;
             movingTo   = movingFrom;
@@ -52,7 +140,7 @@ public class PlayerScript : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void FixedUpdate () {
 
     }
 }
