03e495db3cea9444f3661bf9fb1a6cf9344efe1f
Erik Stambaugh <erik@erikstambaugh.com>
2016-01-28 20:37:48 -0800

    First part of fixing the pixel resolution issue: Making the camera snap to 1/16 unit

--- Unity/CameraScript.cs
+++ Unity/CameraScript.cs
@@ -5,10 +5,11 @@ public class CameraScript : MonoBehaviour {
 
     public Transform target = null;
     private float smooth = 5;
+    private Vector3 virtual_position = new Vector3(0,0,0);
 
     // Use this for initialization
     void Start () {
-
+        virtual_position = transform.position;
     }
 
     // Update is called once per frame
@@ -16,7 +17,11 @@ public class CameraScript : MonoBehaviour {
 
         if (target)
         {
-            transform.position = new Vector3(Mathf.Lerp(transform.position.x,target.position.x,Time.deltaTime*this.smooth), Mathf.Lerp(transform.position.y,target.position.y,Time.deltaTime*this.smooth), -100);
+            virtual_position = new Vector3(Mathf.Lerp(virtual_position.x,target.position.x,Time.deltaTime*this.smooth), Mathf.Lerp(virtual_position.y,target.position.y,Time.deltaTime*this.smooth), -100);
+            Vector3 new_pos = transform.position;
+            new_pos.x = Mathf.Round(virtual_position.x * 16) / 16;
+            new_pos.y = Mathf.Round(virtual_position.y * 16) / 16;
+            transform.position = new_pos;
         }
 
     }
